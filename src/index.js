3f17e2d2d01ade9505f74fbe47aab48dfb52ff20
Purity Wambua <puritywamb@gmail.com>
2024-08-14 07:40:04 +0300

    react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import routes from "./routes.js";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Login from "./pages/Login";
+// import UserProfile from "./pages/UserProfile";
+// import ErrorPage from "./pages/ErrorPage"
+
+// const router = createBrowserRouter(routes)
+// ([
+//     {
+//         path: "/",
+//         element: <Home />,
+//         errorElement: <ErrorPage />
+//       }, 
+//       {
+//         path: "/about",
+//         element: <About />,
+//         errorElement: <ErrorPage />
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//         errorElement: <ErrorPage />
+//       },
+//       {
+//         path: "/profile/:id",
+//         element: <UserProfile />,
+//         errorElement: <ErrorPage />
+//       }
+//     ]);
+    
+    
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<RouterProvider router={router} />);
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.js";
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
